77a9c795aa97fcc35829d71e3bc3250db1f90144
zzeng31 <zinuozeng@gmail.com>
2023-06-06 18:25:25 -0700

    pizza menu

---
+++ pizza-menu/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../index.css";
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>Fast React Pizza Co.</h1>
+    </header>
+  );
+};
+
+export default Header;
---
+++ pizza-menu/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+// React 18
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
+
+// React before 18
+// React.render(<App/>);
